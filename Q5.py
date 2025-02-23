f67ac7d86f06fedeb3e34d245d34a082fdc3c3fc
Aditya Vimal <139321429+adityavimal15@users.noreply.github.com>
2025-02-23 23:31:20 +0530

    Add files via upload

---
+++ Q5.py
@@ -0,0 +1,4 @@
+# Create a program to find the maximum and minimum elements in a list. 
+list = [10,20,55,5,334,22]
+print("maximum element in list :",max(list))
+print("minimum element in list :",min(list))
